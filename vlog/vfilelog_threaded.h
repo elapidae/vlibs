ee7bd529f1c83d4bc3ef2d94dab7dd856c7e14af
Sasha Gromtsev <elapidae@yandex.ru>
2018-05-28 14:30:50 +0300

    Вырезан namespace vlog из файловых логгеров.

--- vlog/vfilelog_threaded.h
+++ vlog/vfilelog_threaded.h
@@ -6,48 +6,48 @@
 #include <memory>
 
 //=======================================================================================
-namespace vlog
+//namespace vlog
+//{
+//===================================================================================
+class VGroupFileLog_Threaded final
 {
-    //===================================================================================
-    class VGroupFileLog_Threaded final
-    {
-    public:
-        VGroupFileLog_Threaded( const std::string &path,
-                                long one_file_size,
-                                int  rotate_files_count );
-        ~VGroupFileLog_Threaded();
+public:
+    VGroupFileLog_Threaded( const std::string &path,
+                            long one_file_size,
+                            int  rotate_files_count );
+    ~VGroupFileLog_Threaded();
 
-        void execute( const VLogEntry &entry );
+    void execute( const VLogEntry &entry );
 
-        void register_self();
+    void register_self();
 
-        int queued_entries() const;
+    int queued_entries() const;
 
-    private:
-        std::unique_ptr<VGroupFileLog> _log;
-        VThread _thread;
-    };
-    //===================================================================================
-    class VCommonFileLog_Threaded final
-    {
-    public:
-        VCommonFileLog_Threaded( const std::string &fname,
-                                 long one_file_size,
-                                 int  rotate_files_count );
-        ~VCommonFileLog_Threaded();
+private:
+    std::unique_ptr<VGroupFileLog> _log;
+    VThread _thread;
+};
+//===================================================================================
+class VCommonFileLog_Threaded final
+{
+public:
+    VCommonFileLog_Threaded( const std::string &fname,
+                             long one_file_size,
+                             int  rotate_files_count );
+    ~VCommonFileLog_Threaded();
 
-        void execute( const VLogEntry &entry );
+    void execute( const VLogEntry &entry );
 
-        void register_self();
+    void register_self();
 
-        int queued_entries() const;
+    int queued_entries() const;
 
-    private:
-        std::unique_ptr<VCommonFileLog> _log;
-        VThread _thread;
-    };
-    //===================================================================================
-}
+private:
+    std::unique_ptr<VCommonFileLog> _log;
+    VThread _thread;
+};
+//===================================================================================
+//}
 //=======================================================================================
 
 
