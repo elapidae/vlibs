7a1496dc5899052efd51b1dd62f5fadcc5b3e934
Sasha Gromtsev <elapidae@yandex.ru>
2018-05-28 14:16:30 +0300

    Выкинуто namespace vlog, введены прокси.

--- vlog/vlog_pretty.h
+++ vlog/vlog_pretty.h
@@ -12,20 +12,27 @@
 #define vwarning VWARNING
 #define vfatal   VFATAL
 //=======================================================================================
+#define vtrace_proxy   VTRACE_PROXY
+#define vdebug_proxy   VDEBUG_PROXY
+#define vdeb_proxy     VDEBUG_PROXY
+#define vrunlog_proxy  VRUNLOG_PROXY
+#define vwarning_proxy VWARNING_PROXY
+#define vfatal_proxy   VFATAL_PROXY
+//=======================================================================================
 
 //=======================================================================================
 class VLogError : public std::exception
 {
 public:
-    explicit VLogError( const vlog::VLogger &e );
-    explicit VLogError( const vlog::VLogEntry &e );
+    explicit VLogError( const VLogger   &logger );
+    //explicit VLogError( const VLogEntry &entry  );
 
     virtual const char    * what()  const noexcept override;
-    const vlog::VLogEntry & entry() const noexcept;
+    const VLogEntry       & entry() const noexcept;
 
 private:
-    std::string     _what;
-    vlog::VLogEntry _entry;
+    std::string _what;
+    VLogEntry   _entry;
 };
 //=======================================================================================
 
--- vlog/vlogger.h
+++ vlog/vlogger.h
@@ -5,58 +5,58 @@
 
 
 //=======================================================================================
-class VLogError;
-namespace vlog
+class VLogger final : public _vcat_iface<VLogger>
 {
-    //===================================================================================
-    class VLogger final : public _vcat_iface<VLogger>
-    {
-    public:
+public:
 
-        //-------------------------------------------------------------------------------
-        using Executer = std::function<void(const VLogEntry &entry)>;
+    //-------------------------------------------------------------------------------
+    using Executer = std::function<void(const VLogEntry &entry)>;
 
-        // Предназначен для создания из макросов.
-        VLogger( VLogEntry::Type,
-                 const std::string &file, int32_t line, const std::string &func );
+    // Предназначен для создания из макросов.
+    VLogger( VLogEntry::Type,
+             const std::string &file, int32_t line, const std::string &func );
 
-        //-------------------------------------------------------------------------------
-        // Здесь регистрируются обработчики, исполнители вхождений логов.
-        // NB! throw_on_fatal(on) было удалено, от него больше вреда, чем пользы.
-        // Вместо подобных "трюков" следует "честно" бросать исключения.
-        // В vlog_pretty.h определен класс VLogError : public std::exception,
-        // которым можно пользоваться, например, так: throw VLogError( vfatal << "msg" );
-        static void add_executer( Executer e );
-        static void clear_executers();
-        static void execute( const VLogEntry &entry );
+    enum class _is_proxy { is_proxy };
+    VLogger( VLogEntry::Type,
+             const std::string &file, int32_t line, const std::string &func,
+             _is_proxy );
+
+    VLogEntry cur_entry() const;
+    operator VLogEntry()  const;
+
+    //-------------------------------------------------------------------------------
+    // Здесь регистрируются обработчики, исполнители вхождений логов.
+    // NB! throw_on_fatal(on) было удалено, от него больше вреда, чем пользы.
+    // Вместо подобных "трюков" следует "честно" бросать исключения.
+    // В vlog_pretty.h определен класс VLogError : public std::exception,
+    // которым можно пользоваться, например, так: throw VLogError( vfatal << "msg" );
+    static void add_executer( Executer e );
+    static void clear_executers();
+    static void execute( const VLogEntry &entry );
 
 
-        static void _log_to_cout( const VLogEntry &entry );
-        static void _log_to_cerr( const VLogEntry &entry );
+    static void _log_to_cout( const VLogEntry &entry );
+    static void _log_to_cerr( const VLogEntry &entry );
 
-        //-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
 
-        ~VLogger();
+    ~VLogger();
 
-        VLogger( VLogger && )               = default;
-        VLogger& operator = ( VLogger && )  = default;
+    VLogger( VLogger && )               = default;
+    VLogger& operator = ( VLogger && )  = default;
 
-        VLogger( const VLogger & )              = delete;
-        VLogger& operator = ( const VLogger & ) = delete;
+    VLogger( const VLogger & )              = delete;
+    VLogger& operator = ( const VLogger & ) = delete;
 
-        //-------------------------------------------------------------------------------
-    private:
-        VLogEntry _entry;
-        vcat      _cat;
+    //-------------------------------------------------------------------------------
+private:
+    VLogEntry _entry;
+    vcat      _cat;
+    bool      _i_am_proxy = false;
 
-        friend class _vcat_iface<VLogger>;
-        template<typename T> void do_cat( const T& val ) { _cat.cat(val); }
-
-        friend class ::VLogError;
-        VLogEntry _get_cur_entry() const;
-    };
-    //===================================================================================
-} // vlog namespace
+    friend class _vcat_iface<VLogger>;
+    template<typename T> void do_cat( const T& val ) { _cat.cat(val); }
+};
 //=======================================================================================
 
 
