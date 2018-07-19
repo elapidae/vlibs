d06d78ac600e1074296685ea1d265d1352ae6ee4
Ekaterina <kitkat52@gmail.com>
2018-07-19 17:26:46 +0300

    added cmake files

---
+++ vimage/vimage_zcm.cmake
@@ -0,0 +1,19 @@
+
+
+message( "Include vimage..." )
+
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11" )
+
+include_directories( "${VLIBS_PATH}/vimage/" )
+
+FILE( GLOB HEADERS_vimage "${VLIBS_PATH}/vimage/vimage_zcm.h"   )
+FILE( GLOB SOURCES_vimage "${VLIBS_PATH}/vimage/vimage_zcm.cpp" )
+
+set ( HEADERS ${HEADERS} ${HEADERS_vimage} )
+set ( SOURCES ${SOURCES} ${SOURCES_vimage} )
+
+set ( INC_ALL ${INC_ALL} ${HEADERS_vimage} )
+set ( SRC_ALL ${SRC_ALL} ${SOURCES_vimage} )
+
+message( "vimage included..." )
+
