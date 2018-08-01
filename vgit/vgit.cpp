a3e031287a5ba290f6c4db74c127150ed3c8836a
Sasha Gromtsev <elapidae@yandex.ru>
2018-08-01 11:53:00 +0300

    VGit -- добавлены автор и, главное, дата коммита.

--- vgit/vgit.cpp
+++ vgit/vgit.cpp
@@ -9,16 +9,39 @@
 //=======================================================================================
 
 //=======================================================================================
-#define VGIT_CURRECT_HASH_BYEL    VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_HASH_ELPD)
-#define VGIT_CURRECT_RVCOUNT_BYEL VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_REVCOUNT_ELPD)
-#define VGIT_CURRECT_BRANCH_BYEL  VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_BRANCH_ELPD)
+#define VGIT_CURRECT_HASH_BYEL      VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_HASH_ELPD)
+#define VGIT_CURRECT_REVCOUNT_BYEL  VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_REVCOUNT_ELPD)
+#define VGIT_CURRECT_BRANCH_BYEL    VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_BRANCH_ELPD)
+#define VGIT_CURRECT_DATE_BYEL      VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_DATE_ELPD)
+#define VGIT_CURRECT_AUTHOR_BYEL    VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_AUTHOR_ELPD)
+//#define VGIT_CURRECT_COMMENT_BYEL   VGIT_STRING_FOR_EXTRACT_DEFINES(VGIT_COMMENT_ELPD)
+//#define VGIT_CURRECT_OLOLOLOLO    VGIT_STRING_FOR_EXTRACT_DEFINES(OLOLOLOLO)
 //=======================================================================================
+
 
 //=======================================================================================
 std::string VGit::hash()
 {
     return VGIT_CURRECT_HASH_BYEL;
 }
+//=======================================================================================
+std::string VGit::date()
+{
+    return VGIT_CURRECT_DATE_BYEL;
+}
+//=======================================================================================
+std::string VGit::author()
+{
+    return VGIT_CURRECT_AUTHOR_BYEL;
+}
+//=======================================================================================
+//  Пока не запускается...
+//std::string VGit::comment()
+//{
+//    return VGIT_CURRECT_OLOLOLOLO;
+//    //return "comment";
+//    return VGIT_CURRECT_COMMENT_BYEL;
+//}
 //=======================================================================================
 std::string VGit::branch()
 {
@@ -27,7 +50,7 @@ std::string VGit::branch()
 //=======================================================================================
 std::string VGit::revcount()
 {
-    return VGIT_CURRECT_RVCOUNT_BYEL;
+    return VGIT_CURRECT_REVCOUNT_BYEL;
 }
 //=======================================================================================
 #pragma GCC diagnostic push
@@ -35,26 +58,31 @@ std::string VGit::revcount()
 std::string VGit::compile_datetime()
 {
     std::stringstream ss;
-    ss << __DATE__ << " " << __TIME__;
+    ss << __DATE__ << "T" << __TIME__;
     return ss.str();
 }
 #pragma GCC diagnostic pop
 //=======================================================================================
 std::string VGit::as_message(const std::string &query)
 {
-    if ( query == "--vgit-hash" )       return hash();
-    if ( query == "--vgit-branch" )     return branch();
+    if ( query == "--vgit-hash"     )   return hash();
+    if ( query == "--vgit-branch"   )   return branch();
     if ( query == "--vgit-revcount" )   return revcount();
+    if ( query == "--vgit-date"     )   return date();
+    if ( query == "--vgit-author"   )   return author();
 
     std::stringstream ss;
-    ss << "hash=" << hash()
-       << ", branch=" << branch()
-       << ", revcount=" << revcount() <<
-          ", ctime=" << compile_datetime();
+    ss << "hash="       << hash()
+       << ", branch="   << branch()
+       << ", revcount=" << revcount()
+       << ", date="     << date()
+       << ", author="   << author()
+       << ", comptime=" << compile_datetime();
+
     return ss.str();
 }
 //=======================================================================================
-void VGit::print_and_exit_if_need(int argc, const char **argv, int retcode)
+void VGit::print_and_exit_if_need(int argc, const char * const *argv, int retcode)
 {
     std::string vgit("--vgit");
     for ( int i = 1; i < argc; ++i )
