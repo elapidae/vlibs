79f24db964d668efee40ffa18a5ab66fa647dc0b
Sasha Gromtsev <elapidae@yandex.ru>
2018-09-01 12:51:07 +0300

    vgio -- изменена 'всасываемая' библиотека с glib на gio, что характерно...

--- vgio/core/core.cmake
+++ vgio/core/core.cmake
@@ -26,10 +26,10 @@ if ( NOT  VGIO_CORE_INCLUDED )
         message( FATAL_ERROR "Нужен pkgconfig для подключения glib." )
     endif()
 
-    pkg_search_module( GLib_PKG REQUIRED glib-2.0 )
+    pkg_search_module( GLib_PKG REQUIRED gio-2.0 )
 
     if ( NOT GLib_PKG_FOUND )
-        message( FATAL_ERROR "glib not found" )
+        message( FATAL_ERROR "gio not found" )
     else()
         message( "=== GLib_PKG was found === " )
     endif()
@@ -39,7 +39,7 @@ if ( NOT  VGIO_CORE_INCLUDED )
     include_directories( ${GLib_PKG_INCLUDE_DIRS} )
 
     set( VGIO_DIR "${VLIBS_DIR}/vgio" )
-    message( "Dont forget use  target_link_libraries({PROJECT_NAME} {V_LIBRARIES}) !!!" )
+    message("Dont forget use target_link_libraries(\${PROJECT_NAME} \${V_LIBRARIES}) !")
     #>>> Stop your code here ------------------------------------------------------------
 
     #include_directories( "${VLIBS_PATH}/core/")
