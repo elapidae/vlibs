5bc2306acc84311cd78b21b6ee293a9d5d715570
Sasha Gromtsev <elapidae@yandex.ru>
2019-01-23 17:39:21 +0300

    HDLC_Parser: добавлен статический метод кодирования пакета, добавлена проверка переполнения, чуть причесан код и комменты.

--- hdlc_parser/hdlc_parser.cpp
+++ hdlc_parser/hdlc_parser.cpp
@@ -37,57 +37,62 @@ HDLC_Parser::Error::Error(const std::string &msg)
 
 
 //=======================================================================================
-void HDLC_Parser::append( const std::string &app_data )
+HDLC_Parser::HDLC_Parser( size_t max_packet_size )
+    : _max_packet_size( max_packet_size )
+{}
+//=======================================================================================
+void HDLC_Parser::append( const std::string& app_data )
 {
     for ( char ch: app_data )
     {
         //  Пока не найдем первый символ 7E -- ничего не читаем.
         if ( !_packet_began )
         {
-            if ( ch == ch_7E ) { _packet_began = true; }
-            else               { lost_from_begin(ch);  }
-
+            if ( ch == ch_7E )  _packet_began = true;
+            else                lost_from_begin( ch );
             continue;
         }
 
         //  Ожидаем только второй символ escape-последовательности.
         if ( _escaped )
         {
-            if      ( ch == ch_5D ) _buffer.append( ch_7D );
-            else if ( ch == ch_5E ) _buffer.append( ch_7E );
+            if      ( ch == ch_5D ) _buf_append( ch_7D );
+            else if ( ch == ch_5E ) _buf_append( ch_7E );
             else    _throw_error( vcat().hex()("Unexpected escape symbol: ", int(ch)) );
             _escaped = false;
             continue;
         }
 
-        //  Нашли начало или конец пакета.
-        if ( ch == ch_7E )
+        //  Разбираемся кто к нам пожаловал.
+        switch ( ch )
         {
-            if ( _escaped )
-                _throw_error( vcat().hex()("Unexpected escape before 0x7E.") );
-
-            if ( !_buffer.empty() )
-                received( _buffer );
-
-
-            _buffer.clear();
-            continue;
+        case ch_7E: _buf_send();        break;  //  Нашли границу пакета.
+        case ch_7D: _escaped = true;    break;  //  Начало escape последовательности.
+        default:    _buf_append( ch );          //  Нормальный символ.
         }
-
-        //  Начало escape последовательности.
-        if ( ch == ch_7D )
-        {
-            _escaped = true;
-            continue;
-        }
-
-        //  Ничего необычного, нормальный символ. Просто кладем в буффер.
-        _buffer.append( ch );
 
     } // for ( char ch: app_data )
 }
 //=======================================================================================
-void HDLC_Parser::_throw_error( const std::string &msg )
+void HDLC_Parser::_buf_append( char ch )
+{
+    if ( _max_packet_size == 0 ) return;        //  Размер не проверяем'c.
+
+    if ( _buffer.size() >= _max_packet_size )
+        _throw_error( "Buffer overflow" );
+
+    _buffer.push_back( ch );
+}
+//=======================================================================================
+void HDLC_Parser::_buf_send()
+{
+    if ( !_buffer.empty() )
+        received( _buffer );
+
+    _buffer.clear();
+}
+//=======================================================================================
+[[noreturn]] void HDLC_Parser::_throw_error( const std::string &msg )
 {
     _buffer.clear();
     _packet_began = false; // Обозначает что нашли в последовательности первый 7E.
@@ -95,4 +100,47 @@ void HDLC_Parser::_throw_error( const std::string &msg )
 
     throw Error( msg );
 }
+//=======================================================================================
+
+
+//=======================================================================================
+std::string HDLC_Parser::encode( const std::string& packet )
+{
+    std::string res;
+    for ( char ch: packet )
+    {
+        switch ( ch )
+        {
+        case ch_7E:
+            res.push_back( ch_7D );
+            res.push_back( ch_5E );
+            break;
+        case ch_7D:
+            res.push_back( ch_7D );
+            res.push_back( ch_5D );
+            break;
+        default:
+            res.push_back( ch );
+        }
+    }
+    return res;
+}
+//=======================================================================================
+
+/*
+//  Для кодирования на ардуинках.
+static void hdlc_send( char ch )
+{
+    static const char ch_7E[] = { 0x7D, 0x5E };
+    static const char ch_7D[] = { 0x7D, 0x5D };
+
+    switch ( ch )
+    {
+    case 0x7E:  Serial.write( ch_7E, 2 );   break;
+    case 0x7D:  Serial.write( ch_7D, 2 );   break;
+    default:    Serial.write( ch );
+    }
+}
+*/
+
 //=======================================================================================
